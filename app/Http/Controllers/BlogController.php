46a15f974bda72c7b47db4174020d8fb5260d583
Vetal <vetal@gmail.com>
2017-12-02 16:23:47 +0200

    base project (all files without vendor)

---
+++ app/Http/Controllers/BlogController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Http\Requests;
+use App\Post;
+
+class BlogController extends Controller {
+
+    public function getIndex() {
+
+        $posts = Post::paginate(51);
+
+//        dd($posts);
+//        var_dump($posts);
+
+        return view('blog.index')->withPosts($posts);
+    }
+
+    public function getSingle($slug) {
+        // fetch from the DB based on slug
+        $post = Post::where('slug', '=', $slug)->first();
+//        dd($post);
+        // return the view and pass in the post object
+        return view('blog.single')->withPost($post);
+    }
+
+}
---
+++ database/seeds/PostsTableSeeder.php
@@ -0,0 +1,19 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class PostsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('posts')->insert([
+            'title' => 'sdfs',
+            'body' => 'dffdfg',
+        ]);
+    }
+}
